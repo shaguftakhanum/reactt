007203588b8b6a758f74f7c3a8b5e95791fe254f
shugufta khanum <shaguftakahnum900@gmail.com>
2022-09-05 06:14:17 -0700

    uploaded

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,70 +1,72 @@
 import React from "react";
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Layout from "../components/Layout/Layout";
 import { Link } from "react-router-dom";
-const Blog=()=>{
-    let[items,setitem]=useState([]);
-    useEffect(() => {
-        axios.get('http://localhost:8000/api/blogs/getall')
-        .then(res=>{
-			// console.log('res =>',res);
-			// console.log('data =>',res.data.data)
-			setitem(res.data.data);
-		}).catch(err=>{
-			console.log(err)
-		})
+const Blog = () => {
+	let [items, setitem] = useState([]);
+	useEffect(() => {
+		axios.get('http://localhost:8000/api/blogs/getall')
+			.then(res => {
+				// console.log('res =>',res);
+				// console.log('data =>',res.data.data)
+				setitem(res.data.data);
+			}).catch(err => {
+				console.log(err)
+			})
 
-        }, []);
-
-
-    return(
-        <>
-    <Layout>
+	}, []);
 
 
-					 <table className="table table-striped">
-						<thead>
-							<tr>
-								<th>Id</th>
-								<th>Name</th>
-								<th>Type</th>
-								<th>Type_id</th>
-								<th>CreatedAt</th>
-								<th>UpdatedAt</th>
-								<th>Actions</th>
-
-
-
-							</tr>
-						</thead>
-						<tbody>
-							{
-								items.map((item) => (
-
-									<tr>
-										<td>{item.id}</td>
-										<td>{item.name}</td>
-										<td>{item.type}</td>
-										<td>{item.type_id}</td>
-										<td>{item.createdAt}</td>
-										<td>{item.updatedAt}</td>
-										<td>
-
-											<button type="button" className="btn btn-success">Edit<i className="fas fa-edit"></i></button>
-											<button type="button" className="btn btn-danger">Delete<i className="far fa-trash-alt"></i></button></td>
-									</tr>
-								))
-							}
-						</tbody>
-					</table>
-					<Link to="/blog/create" element="<Create/>" >
+	return (
+		<>
+			<Layout>
+				<Link to="/blog/create" element="<Create/>" >
 					<h1>post request</h1>
-					</Link>
+				</Link>
 
-    </Layout>
-    </>
-    );
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>Name</th>
+							<th>Type</th>
+							<th>Type_id</th>
+							<th>CreatedAt</th>
+							<th>UpdatedAt</th>
+							<th>Actions</th>
+
+
+
+						</tr>
+					</thead>
+					<tbody>
+						{
+							items.map((item) => (
+
+								<tr>
+									<td>{item.id}</td>
+									<td>{item.name}</td>
+									<td>{item.type}</td>
+									<td>{item.type_id}</td>
+									<td>{item.createdAt}</td>
+									<td>{item.updatedAt}</td>
+									<td>
+
+										<button type="button" className="btn btn-success">Edit<i className="fas fa-edit"></i></button>
+										<button type="button" className="btn btn-danger">Delete<i className="far fa-trash-alt"></i></button></td>
+								</tr>
+							))
+						}
+					</tbody>
+				</table>
+				<Link to="/blog/create" element="<Create/>" >
+					<h1>post request</h1>
+				</Link>
+
+			</Layout>
+		</>
+	);
 
 }
 export default Blog;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,147 +1,67 @@
-// import React, { useState } from 'react'
-// import axios from 'axios'
-// const Create = () => {
-//     const url = "http://localhost:8000/api/blogs/create";
-//     const initialUserState = {
-//         name: "",
-//         photos:[]
-//     }
-
-//     const [data, setdata] = useState(initialUserState)
-
-//     // const [data, setdata] = useState([{
-//     //     name: "",
-//     //     photos:[]
-//     // }])
-//     function onsubmit(e) {
-//         e.preventDefault();
-//         // const target = e.target;
-//         // const data = new FormData(target);
-//         // console.log(data)
-//         axios.post(url, {
-//             name: data.name,
-//             photos: data.photos
-
-
-//         }).then(res => {
-//             // console.log(res);
-//             // console.log(res.data);
-//             // console.log('res =>', res)
-//             const target = e.target;
-//         const data = new FormData(target);
-//         console.log(data)
-
-
-
-//         }).catch(err => {
-//             console.log(err)
-//         })
-//         // console.log('data =>', data)
-//         console.log(JSON.stringify(data))
-//         // console.log('data =>', data)
-
-//     }
-
-
-//     function handle(e) {
-//         const newdata = { ...data };
-//         newdata[e.target.id] = e.target.value;
-//         // newdata[e.target.files]=e.target.value;
-//         setdata(newdata);
-//         console.log(newdata);
-//     }
-//     function handleImageUpload(e) {
-//         [...e.target.files].forEach(file => {
-//             console.log("file >>> ", file)
-//             console.log(e.target.files[0], "ssss");
-
-//             setdata({
-//                 ...data,
-//                 ...{
-//                     data: file,
-//                     url: URL.createObjectURL(file)
-//                 }
-//         })
-
-//             console.log("data >> ", data)
-//         })
-
-
-
-//     }
-//     // console.log('data =>', data)
-//     return (
-//         <div>
-//             <form onSubmit={(e) => onsubmit(e)} method="post">
-//                 <input onChange={(e) => handle(e)} value={data.name} id="name" type="text" placeholder='name' />
-//                 <input type="file" name="photo" id="photo" multiple value={data.photos}
-//                     onChange={(e) => handleImageUpload(e)}
-//                     accept="image/*" />
-//                 <button>submit</button>
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default Create;
-
 import React, { Component } from "react";
 import axios from 'axios';
 import Layout from "../components/Layout/Layout";
 class Create extends Component {
-    state={
-        name:"",
-        file:[]
+    state = {
+        file: []
     }
-    handlefile(e){
-    let file=e.target.files[0];
-    this.setState({file:file});
-    console.log('file=>',file)
-    }
-    handleclick(e){
-        // console.log(this.state,"bswhb...")
-        // console.log("data =>",this.handleclick)
-        let file=this.state.file;
-        let name=this.state.name;
-        let formData=new FormData();
-        formData.append('image',file);
-        formData.append('name',name)
-        console.log('name=>',name)
+    handlefile(e) {
+        let file = e.target.files[0];
+        let formData = new FormData();
+        formData.append('uploads', file);
 
         axios({
             method: 'post',
-            url: 'http://localhost:8000/api/blogs/create',
-            data:formData
+            url: 'http://localhost:8000/uploads',
+            data: formData
         })
-        .then(function (response) {
-            console.log("response=>",response);
+            .then(function ({data}) {
+                console.log("data=>", data);
 
+            })
+            .catch(function (error) {
+                console.log("error=>", error);
+            });
+        // this.setState({ file: file });
+        // console.log('file=>', file)
+    }
+    handleclick(e) {
+        // console.log("Im here");
+        // console.log(this.state,"bswhb...")
+        // console.log("data =>",this.handleclick)
+        let file = this.state.file;
+        let formData = new FormData();
+        formData.append('uploads', file);
+        console.log('file=> ', file)
+
+        axios({
+            method: 'post',
+            url: 'http://localhost:8000/api/file/create',
+            data: formData
         })
-        .catch(function (error) {
-            console.log("error=>",error);
-        });
+            .then(function (response) {
+                console.log("response=>", response);
+
+            })
+            .catch(function (error) {
+                console.log("error=>", error);
+            });
     }
 
     render() {
         return (
             <>
-            <Layout>
-                <div className="Create">
-                <h1>Form</h1>
-                <div>
-                <form>
-                <label >Name</label><br/>
-                <input type="text" name="name" onChange={(e)=>this.handlefile(e)} />
-                <br/>
-                <label >select file</label><br/>
-                <input type="file" name="file" onChange={(e)=>this.handlefile(e)} />
-                <button   onClick={(e)=>this.handleclick(e)}>uploads</button>
-               </form>
-                </div>
-
-
-
-                </div>
+                <Layout>
+                    <div className="Create">
+                        <h1>Form</h1>
+                        <div>
+                            <form>
+                                <label >select file</label><br />
+                                <input  type="file" multiple name="file" onChange={(e) => this.handlefile(e)} />
+                                <button onClick={(e) => this.handleclick(e)} type="button" className="btn">uploads</button>
+                            </form>
+                        </div>
+                    </div>
                 </Layout>
 
             </>
